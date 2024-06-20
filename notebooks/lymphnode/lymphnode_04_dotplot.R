5467f663fef7c016f7ff56709d1072a7c4b180c6
Linan Zhang <37935731+linanzhang@users.noreply.github.com>
2024-06-20 11:24:43 +0800

    Add files via upload

---
+++ notebooks/lymphnode/lymphnode_04_dotplot.R
@@ -0,0 +1,144 @@
+setwd("~/Desktop/Pitt/Project_sagan_stan/Code_Linan/April/notebooks/lymphnode")
+library(ggplot2)
+library(ggrepel)
+library(Seurat)
+library(dplyr)
+library(RColorBrewer)
+
+FONTSIZE = 25
+RELSIZE = 0.8
+
+# ========== ========== ========== ========== ========== ==========  
+# STAN
+# ========== ========== ========== ========== ========== ==========  
+
+data = read.csv('../df2plot_inR/lymphnode_tf_by_celltype.csv', row.names = 1)
+threshold = 10
+data$p_adj = -log10(data$p_adj+1e-10)
+data$Score = pmax(pmin(data$coef, threshold+2), -threshold-2)
+data$ct = factor(x = data$ct, 
+                      levels = c('NKT', 'Monocytes', 'VSMC', 'B_Cycling', 'B_IFN', 'T_TfR', 'T_Treg',
+                                 'B_plasma', 'Macrophages', 'B_mem', 'DC', 'FDC', 'B_GC', 'B_preGC',
+                                 'T_TIM3+', 'B_activated', 'NK', 'T_CD8+', 'ILC', 'T_CD4+'))
+data$tf = factor(x = data$tf, 
+                      levels = c('PBX1', 'PPARG', 'TCF21', 'STAT1', 'FOXP2', 'PGR', 'IRF1', 'IRF2',
+                                 'STAT2', 'E2F7', 'ETS1', 'GTF2B', 'NR5A2', 'BCL11A', 'RFX3', 'SPIB',
+                                 'EBF1', 'CREB1', 'PAX5', 'BACH2', 'POU2F2', 'STAT3', 'STAT4', 'KLF1',
+                                 'CDX2', 'FOXP3', 'TBX21', 'MYB', 'STAT5B', 'ETV6', 'MAX', 'KMT2A',
+                                 'E2F1', 'FOXM1'))
+
+p = ggplot(data, aes(y=tf, x=ct)) + 
+  geom_point(aes(size=p_adj, color=Score)) +
+  scale_color_gradientn('STAN\nTF\nActivity\nScore',
+                        colors=brewer.pal(11,'PiYG') %>% rev(),
+                        breaks=seq(-threshold,threshold,5), limits=c(-threshold-2,threshold+2)) +
+  theme_classic() +
+  labs(size='Significance') +
+  theme(panel.grid = element_blank(),
+        strip.placement = "outside",
+        strip.background = element_rect(colour="grey", fill="white"),
+        strip.text.y.left = element_text(face='bold', size = rel(RELSIZE), angle = 0),
+        plot.title = element_text(hjust = 0.5, size = rel(RELSIZE)),
+        legend.title = element_text(size = rel(RELSIZE^2)),
+        legend.text = element_text(size = rel(RELSIZE^2)),
+        legend.key.height = unit(0.8,"cm"),
+        axis.title = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        text = element_text(size=FONTSIZE)) +
+  scale_size_area(breaks=c(1, 2, 3, 4),
+                  labels = c("ns","*","**", '***')) 
+
+p
+pdf('../df2plot_inR/lymphnode_dotplot_tf.pdf', width=9, height=12)
+print(p)
+dev.off()
+
+# ========== ========== ========== ========== ========== ==========  
+# gene
+# ========== ========== ========== ========== ========== ==========  
+
+data = read.csv('../df2plot_inR/lymphnode_gene_by_celltype.csv', row.names = 1)
+threshold = 5
+data$p_adj = -log10(data$p_adj+1e-10)
+data$Score = pmax(pmin(data$coef, threshold+1), -threshold-1)
+data$ct = factor(x = data$ct, 
+                      levels = c('NKT', 'Monocytes', 'VSMC', 'B_Cycling', 'B_IFN', 'T_TfR', 'T_Treg',
+                                 'B_plasma', 'Macrophages', 'B_mem', 'DC', 'FDC', 'B_GC', 'B_preGC',
+                                 'T_TIM3+', 'B_activated', 'NK', 'T_CD8+', 'ILC', 'T_CD4+'))
+data$tf = factor(x = data$tf, 
+                      levels = c('PBX1', 'PPARG', 'TCF21', 'STAT1', 'FOXP2', 'PGR', 'IRF1', 'IRF2',
+                                 'STAT2', 'E2F7', 'ETS1', 'GTF2B', 'NR5A2', 'BCL11A', 'RFX3', 'SPIB',
+                                 'EBF1', 'CREB1', 'PAX5', 'BACH2', 'POU2F2', 'STAT3', 'STAT4', 'KLF1',
+                                 'CDX2', 'FOXP3', 'TBX21', 'MYB', 'STAT5B', 'ETV6', 'MAX', 'KMT2A',
+                                 'E2F1', 'FOXM1'))
+
+p = ggplot(data, aes(y=tf, x=ct)) + 
+  geom_point(aes(size=p_adj, color=Score)) +
+  scale_color_gradientn('mRNA\nExpression\nScore',
+                        colors=brewer.pal(11,'RdBu') %>% rev(),
+                        breaks=seq(-threshold,threshold,2.5), limits=c(-threshold-1,threshold+1)) +
+  theme_classic() +
+  labs(size='Significance') +
+  theme(panel.grid = element_blank(),
+        strip.placement = "outside",
+        strip.background = element_rect(colour="grey", fill="white"),
+        strip.text.y.left = element_text(face='bold', size = rel(RELSIZE), angle = 0),
+        plot.title = element_text(hjust = 0.5, size = rel(RELSIZE)),
+        legend.title = element_text(size = rel(RELSIZE^2)),
+        legend.text = element_text(size = rel(RELSIZE^2)),
+        legend.key.height = unit(0.8,"cm"),
+        axis.title = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        text = element_text(size=FONTSIZE)) +
+  scale_size_area(breaks=c(1, 2, 3, 4),
+                  labels = c("ns","*","**", '***')) 
+
+p
+pdf('../df2plot_inR/lymphnode_dotplot_gene.pdf', width=9, height=12)
+print(p)
+dev.off()
+
+# ========== ========== ========== ========== ========== ==========  
+# decoupler
+# ========== ========== ========== ========== ========== ==========   
+
+data = read.csv('../df2plot_inR/lymphnode_tf_by_celltype_decoupler.csv', row.names = 1)
+threshold = 10
+data$p_adj = -log10(data$p_adj+1e-10)
+data$Score = pmax(pmin(data$coef, threshold+2), -threshold-2)
+data$ct = factor(x = data$ct, 
+                      levels = c('NKT', 'Monocytes', 'VSMC', 'B_Cycling', 'B_IFN', 'T_TfR', 'T_Treg',
+                                 'B_plasma', 'Macrophages', 'B_mem', 'DC', 'FDC', 'B_GC', 'B_preGC',
+                                 'T_TIM3+', 'B_activated', 'NK', 'T_CD8+', 'ILC', 'T_CD4+'))
+data$tf = factor(x = data$tf, 
+                      levels = c('PBX1', 'PPARG', 'TCF21', 'STAT1', 'FOXP2', 'PGR', 'IRF1', 'IRF2',
+                                 'STAT2', 'E2F7', 'ETS1', 'GTF2B', 'NR5A2', 'BCL11A', 'RFX3', 'SPIB',
+                                 'EBF1', 'CREB1', 'PAX5', 'BACH2', 'POU2F2', 'STAT3', 'STAT4', 'KLF1',
+                                 'CDX2', 'FOXP3', 'TBX21', 'MYB', 'STAT5B', 'ETV6', 'MAX', 'KMT2A',
+                                 'E2F1', 'FOXM1'))
+
+p = ggplot(data, aes(y=tf, x=ct)) + 
+  geom_point(aes(size=p_adj, color=Score)) +
+  scale_color_gradientn('decoupleR\nTF\nActivity\nScore',
+                        colors=brewer.pal(11,'PiYG') %>% rev(),
+                        breaks=seq(-threshold,threshold,5), limits=c(-threshold-2,threshold+2)) +
+  theme_classic() +
+  labs(size='Significance') +
+  theme(panel.grid = element_blank(),
+        strip.placement = "outside",
+        strip.background = element_rect(colour="grey", fill="white"),
+        strip.text.y.left = element_text(face='bold', size = rel(RELSIZE), angle = 0),
+        plot.title = element_text(hjust = 0.5, size = rel(RELSIZE)),
+        legend.title = element_text(size = rel(RELSIZE^2)),
+        legend.text = element_text(size = rel(RELSIZE^2)),
+        legend.key.height = unit(0.8,"cm"),
+        axis.title = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        text = element_text(size=FONTSIZE)) +
+  scale_size_area(breaks=c(1, 2, 3, 4),
+                  labels = c("ns","*","**", '***')) 
+
+p
+pdf('../df2plot_inR/lymphnode_dotplot_tf_decoupler.pdf', width=9, height=12)
+print(p)
+dev.off()
---
+++ notebooks/lymphnode/lymphnode_04_volcano.R
@@ -0,0 +1,121 @@
+setwd("~/Desktop/Pitt/Project_sagan_stan/Code_Linan/April/notebooks/lymphnode")
+library(ggplot2)
+library(ggrepel)
+library(Seurat)
+library(dplyr)
+
+FONTSIZE = 25
+RELSIZE = 0.8
+
+# ========== ========== ========== ========== ========== ==========  
+# STAN
+# ========== ========== ========== ========== ========== ==========  
+
+dedf = read.csv('../df2plot_inR/lymphnode_dedf_tf.csv', row.names = 1)
+tfs = rownames(dedf)
+n = length(tfs)
+dedf$order = seq(1,n)
+dedf$label = ''
+dedf$color = ''
+for (i in seq(1,n)){
+  if (dedf$pvals_adj[i]<1e-50 & abs(dedf$diff[i])>0.015){
+    dedf$color[i] = 'diff'
+    dedf$label[i] = tfs[i] 
+  }
+}
+
+p = ggplot(dedf) + geom_point(aes(x = diff, y = -log10(pvals_adj), colour = color), size = 2.5) +
+  geom_text_repel(aes(x = diff, y = -log10(pvals_adj), label = label), size = 6, max.overlaps = Inf) +
+  scale_colour_manual(values = c("#BBBBBB" , "#882E72")) + theme_bw() +
+  xlab("Mean Activity Difference\n<- Other                    Germinal Centers ->") +
+  ylab("-log10(pvals_adj)") +
+  xlim(-0.06, 0.06) + 
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold', size = rel(RELSIZE)),
+        legend.title = element_blank(),
+        axis.title = element_text(size = rel(RELSIZE)),
+        text = element_text(size=FONTSIZE)) + 
+  ggtitle('TFs Activities by Germinal Centers\nInferred by STAN')  + NoLegend() 
+p
+
+pdf('../df2plot_inR/lymphnode_volcano_tf.pdf', width=9, height=7.2)
+print(p)
+dev.off()
+
+# ========== ========== ========== ========== ========== ==========  
+# gene
+# ========== ========== ========== ========== ========== ==========  
+
+tfs = read.csv('../df2plot_inR/lymphnode_dedf_tf.csv', row.names = 1) %>% rownames() 
+dedf_gene = read.csv('../df2plot_inR/lymphnode_dedf_gene.csv', row.names = 1)
+dedf_gene = dedf_gene[rownames(dedf_gene) %>% intersect(tfs),]
+
+genes = rownames(dedf_gene)
+n = length(genes)
+dedf_gene$order = seq(1,n)
+dedf_gene$label = ''
+dedf_gene$color = ''
+for (i in seq(1,n)){ 
+  if (dedf_gene$pvals_adj[i]<1e-25 & abs(dedf_gene$diff[i])>1){
+    dedf_gene$label[i] = genes[i] 
+    dedf_gene$color[i] = 'diff'
+  }}
+
+q = ggplot(dedf_gene) + geom_point(aes(x = diff, y = -log10(pvals_adj), colour = color), size = 2.5) +
+  geom_text_repel(aes(x = diff, y = -log10(pvals_adj), label = label), size = 6, max.overlaps = Inf) +
+  scale_colour_manual(values = c("#BBBBBB" , "#f97b72")) + theme_bw() +
+  xlab("Mean Expression Difference\n<- Other                    Germinal Centers ->") +
+  ylab("-log10(pvals_adj)") +
+  xlim(-3, 3) + 
+  # ylim(-0.1, 120) +
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold', size = rel(RELSIZE)),
+        legend.title = element_blank(),
+        axis.title = element_text(size = rel(RELSIZE)),
+        text = element_text(size=FONTSIZE)) + 
+  ggtitle('mRNA Expressions by Germinal Centers')  + NoLegend() 
+
+q
+pdf('../df2plot_inR/lymphnode_volcano_gene.pdf', width=9, height=7.2)
+print(q)
+dev.off()
+
+# ========== ========== ========== ========== ========== ==========  
+# decoupler
+# ========== ========== ========== ========== ========== ==========  
+
+dedf = read.csv('../df2plot_inR/lymphnode_dedf_tf_decoupler.csv', row.names = 1)
+tfs = rownames(read.csv('../df2plot_inR/lymphnode_dedf_tf.csv', row.names = 1))
+dedf = dedf[rownames(dedf) %>% intersect(tfs),]
+
+tfs = rownames(dedf)
+n = length(tfs)
+dedf$order = seq(1,n)
+dedf$label = ''
+dedf$color = ''
+for (i in seq(1,80)){ 
+  if (dedf$pvals_adj[i]<1e-50 & abs(dedf$diff[i])>1){
+    dedf$label[i] = tfs[i] 
+    dedf$color[i] = 'diff'
+  }}
+for (i in seq(n-19,n)){
+  if (dedf$pvals_adj[i]<1e-25 & abs(dedf$diff[i])>0.5){
+    dedf$label[i] = tfs[i] 
+    dedf$color[i] = 'diff'
+  }}
+
+p = ggplot(dedf) + geom_point(aes(x = diff, y = -log10(pvals_adj), colour = color), size = 2.5) +
+  geom_text_repel(aes(x = diff, y = -log10(pvals_adj), label = label), size = 6, max.overlaps = Inf) +
+  scale_colour_manual(values = c("#BBBBBB" , "#882E72")) + theme_bw() +
+  xlab("Mean Activity Difference\n<- Other                    Germinal Centers ->") +
+  ylab("-log10(pvals_adj)") +
+  xlim(-3, 4.5) + ylim(-0.1, 200) +
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold', size = rel(RELSIZE)),
+        legend.title = element_blank(),
+        axis.title = element_text(size = rel(RELSIZE)),
+        text = element_text(size=FONTSIZE)) + 
+  ggtitle('TFs Activities by Germinal Centers\nInferred by decoupleR')  + NoLegend() 
+p
+
+pdf('../df2plot_inR/lymphnode_volcano_tf_decoupler.pdf', width=9, height=7.2)
+print(p)
+dev.off()
+
